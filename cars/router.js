d6b3f8ee10c77fc7320823c0db44d0391f3ee7cb
ofega <fegaeze@gmail.com>
2019-11-06 08:26:05 +0100

    get from db worksgit add .!

--- cars/router.js
+++ cars/router.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const db = require('./db');
 
+router.get('/', async (req, res, next) => {
+    try {
+      const cars = await db.get();
+      res.status(200).json(cars);
+    } catch (error) {
+      next(error);
+    }
+});
 
 
 module.exports = router;
