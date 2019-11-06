22e274daf16acf3ae7d06fc2f3a2ca336f22153d
ofega <fegaeze@gmail.com>
2019-11-06 08:28:59 +0100

    getbyId and post done

--- cars/router.js
+++ cars/router.js
@@ -12,5 +12,23 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.get('/:id', async (req, res, next) => {
+    try {
+      const car = await db.getById(req.params.id);
+      res.status(200).json(car);
+    } catch (error) {
+      next(error);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+      const car = await db.insert(req.body);
+      res.status(200).json(car);
+    } catch (error) {
+      next(error);
+    }
+});
+
 
 module.exports = router;
