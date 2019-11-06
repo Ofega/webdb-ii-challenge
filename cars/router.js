71e4bf9487175e90c82f5fd1167678f817237259
ofega <fegaeze@gmail.com>
2019-11-06 08:52:27 +0100

    improved post endpoint

--- cars/router.js
+++ cars/router.js
@@ -4,40 +4,42 @@ const router = express.Router();
 const db = require('./db');
 
 router.get('/', async (req, res, next) => {
-    try {
-      const cars = await db.get();
-      res.status(200).json(cars);
-    } catch (error) {
-      next(error);
-    }
-});
+  try {
+    const cars = await db.get();
+    res.status(200).json(cars);
+  } catch (error) {
+    next(error);
+  }
+})
 
 router.get('/:id', async (req, res, next) => {
-    try {
-      const car = await db.getById(req.params.id);
-      res.status(200).json(car);
-    } catch (error) {
-      next(error);
-    }
-});
+  try {
+    const car = await db.getById(req.params.id);
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
+})
 
 router.post('/', async (req, res, next) => {
-    try {
-      const car = await db.insert(req.body);
-      res.status(200).json(car);
-    } catch (error) {
-      next(error);
-    }
-});
+  const { VIN, make, model, mileage, transmissionType, statusOfTitle } = req.body;
+
+  try {
+    const car = await db.insert({ VIN, make, model, mileage, transmissionType, statusOfTitle });
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
+})
 
 router.use((error, req, res) => {
-    res.status(500).json({
-        file: 'router',
-        method: req.method,
-        url: req.url,
-        message: error.message
-    })
-});
+  res.status(500).json({
+      file: 'router',
+      method: req.method,
+      url: req.url,
+      message: error.message
+  })
+})
   
 
 module.exports = router;
